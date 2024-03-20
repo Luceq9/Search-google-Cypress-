49ac82af02b5bd8f22aa0712fad613529a445a5c
Luceq <lucjanjaroszewski@gmail.com>
2024-03-20 10:52:43 +0100

    Cypress test google search with visual test in command

---
+++ cypress/integration/youtube/Test_home.spec.js
@@ -0,0 +1,14 @@
+describe('Test Home', () => {
+	it('Visit Home', () => {
+		cy.visit('https://www.google.com/')
+		cy.get('#L2AGLb').click()
+		cy.get('textarea.gLFyf').type('Mercedes 190{enter}')
+		cy.get('.GKS7s')
+			.eq(0)
+			.click()
+
+		/* Visual Testing (Percy.io)
+        cy.percySnapshot('mercedes')
+        */
+	})
+})
