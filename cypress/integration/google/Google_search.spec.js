c3eecc519786a93453ad50960f351c7542e578e2
Luceq <lucjanjaroszewski@gmail.com>
2024-03-21 12:27:29 +0100

    Change package.json

--- cypress/integration/google/Google_search.spec.js
+++ cypress/integration/google/Google_search.spec.js
@@ -1,12 +1,16 @@
+// Easy test in cypress for google search
+// Start app: npm run open
+// Author: Luceq
+
 describe('Test Home', () => {
 	it('Visit Home', () => {
 		cy.visit('https://www.google.com/')
 		cy.get('#L2AGLb').click()
 		cy.get('textarea.gLFyf').type('Mercedes 190{enter}')
+		//  this cy.get('.YmvwI').eq(0).click() or cy.get('.GKS7s').eq(0).click()
 		cy.get('.GKS7s')
 			.eq(0)
 			.click()
-
 		/* Visual Testing (Percy.io)
         cy.percySnapshot('mercedes')
         */
